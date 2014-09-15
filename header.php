3597a9276d7ee44c8612d2f55605756b8bedc7d2
Emilie COUDRAT <coudrat.emilie@gmail.com>
2014-09-15 14:38:33 +0200

    15092012 avec PHP
    intégration index.php et header.php

---
+++ header.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<meta name="description" content="<?php echo $meta_description; ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Archivo+Black' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div class="container-fluid wrapper">
+	<a href="#">
+		<header>
+	<!--la div header-->
+			<div class="header">
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="titres">
+							<li>
+							<h1>PLAID</h1>
+							<h2>Le site où la <span id="lose">lose</span> se sent bien.</h2>
+							</li>
+						</ul>
+					</div>
+				</div>	
+			</div>
+		</header>	
+	</a> 
+
+<nav>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid container-navbar">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html"><li><p>home</p></li></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      	<ul class="nav navbar-nav">
+        	<li><a href="article.html"><p>en mode plaid</p></a></li>
+        	<li><a href="#"><p>we are family</p></a></li>
+        	<li><a href="https://twitter.com/intent/tweet?hashtags=plaidfilter%2C&original_referer=&related=jasoncosta&share_with_retweet=never&text=My%20story%20is%20about%20what%27s%20happening%20today&tw_p=tweetbutton"><p>#plaidfilter</p></a></li>
+        	<li><a href="#"><p>devenir plaider</p></a></li>
+		</ul>
+	</div>	
+</div>		
+</nav>
